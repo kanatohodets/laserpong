b804fb9520dd732fc56e98f1d940d9cabfa6a1e9
Marcus Huderle <huderlem@carleton.edu>
2013-05-05 17:25:40 -0500

    Added Bobby to authors.

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
     t.title = "LazerPong"
-    t.author = "Marcus Huderle, Ben Tyler, Kiley Maki"
+    t.author = "Marcus Huderle, Ben Tyler, Kiley Maki, Bobby Davis"
     t.url = "http://apps.carleton.edu/student/orgs/gamedev/"  
     t.screen.width = 900
     t.screen.height = 750
